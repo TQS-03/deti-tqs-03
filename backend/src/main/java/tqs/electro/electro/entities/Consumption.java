9398089c12649af07bc0aa63316f9172ba639f79
jmendes418 <j.m.mendes@ua.pt>
2025-06-02 00:19:29 +0100

    feat(frontend): functional authentication

--- backend/src/main/java/tqs/electro/electro/entities/Consumption.java
+++ backend/src/main/java/tqs/electro/electro/entities/Consumption.java
@@ -2,9 +2,7 @@ package tqs.electro.electro.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -13,8 +11,6 @@ import java.util.UUID;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class Consumption {
 
     @Id
